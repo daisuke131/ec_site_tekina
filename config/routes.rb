b7072b4a17014f33cc624d7fd15a3bd2a0e7b1b7
daisuke <dada.00131@google.com>
2019-04-09 22:00:03 +0900

    ログインパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   post "/favorite/:id" => "favorites#favorite", as: "favorite"
   delete "/favorite/:id" => "favorites#del_favorite", as: "del_favorite"
   get "search" => "products#search"
-  devise_for :users
+  devise_for :users, skip: :session
+  devise_scope :user do
+      get 'login' => 'devise/sessions#new', as: :new_user_session
+      post 'login' => 'devise/sessions#create', as: :user_session
+      delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
   resources :users, only: [:show]
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
