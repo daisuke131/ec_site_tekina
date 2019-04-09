20a881bcfd7c933d518a287bec329aaf786ead17
daisuke <dada.00131@google.com>
2019-04-09 21:06:49 +0900

    heroku用

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get "search" => "products#search"
   devise_for :users
   resources :users, only: [:show]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
