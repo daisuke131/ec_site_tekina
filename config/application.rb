ce47d96484babd821b68a2a297b83bdfedd768e9
daisuke <dada.00131@google.com>
2019-04-18 15:25:24 +0900

    heroku Precompiling assets failed. エラー対応

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module EcSiteTekina
     config.i18n.default_locale = :ja
     config.assets.paths << config.root.join("vendor/assets/javascripts")
     config.assets.paths << config.root.join("vendor/assets/stylesheets")
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
