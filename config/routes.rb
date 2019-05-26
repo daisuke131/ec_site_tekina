c9bc74a30ba65461d7046f3bd7f2af7bed791ced
daisuke <dada.00131@google.com>
2019-05-26 22:22:33 +0900

    routes.rb修正 arrow記法からto記法へ

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "products#index"
-  resources :products
-  post "products/:id/charge" => "products#charge", as: "charge"
-  get "users/:id/favorites" => "users#favorites", as: "user_favorite"
-  get "users/:id/buys" => "users#buys", as: "user_buy"
-  get "users/:id/solds" => "users#solds", as: "user_sold"
-  post "/favorite/:id" => "favorites#favorite", as: "favorite"
-  delete "/favorite/:id" => "favorites#del_favorite", as: "del_favorite"
-  get "search" => "products#search"
+  resources :products, except: [:index]
+  post "products/:id/charge", to: "products#charge", as: "charge"
+  get "users/:id/favorites", to: "users#favorites", as: "user_favorite"
+  get "users/:id/buys", to: "users#buys", as: "user_buy"
+  get "users/:id/solds", to: "users#solds", as: "user_sold"
+  post "/favorite/:id", to: "favorites#favorite", as: "favorite"
+  delete "/favorite/:id", to: "favorites#del_favorite", as: "del_favorite"
+  get "search", to: "products#search"
   devise_for :users, skip: :session
   devise_scope :user do
-    get "login" => "devise/sessions#new", as: :new_user_session
-    post "login" => "devise/sessions#create", as: :user_session
-    delete "logout" => "devise/sessions#destroy", as: :destroy_user_session
+    get "login", to: "devise/sessions#new", as: :new_user_session
+    post "login", to: "devise/sessions#create", as: :user_session
+    delete "logout", to: "devise/sessions#destroy", as: :destroy_user_session
   end
   resources :users, only: [:show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
